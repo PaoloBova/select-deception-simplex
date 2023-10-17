8360f7c076d8a174c1d9e628cc6fb1f828c8d9f1
Paolo Bova <paolobova@protonmail.com>
2023-10-17 16:22:19 +0100

    Set demo.py and  demo.ipynb to generate the plots used in the report

--- demo.py
+++ demo.py
@@ -3,24 +3,26 @@ import core
 
 param_list = [
     {"b": 1.5, "c": 0.5, "d": 0.1, "s": 0.2,
-     "Z": 100, "beta": 1, "mu": 1e-3, "strategies": ['CC', 'HD', 'TD']},
-    {"b": 1.5, "c": 0.5, "d": 0.2, "s": 0.2,
-     "Z": 100, "beta": 1, "mu": 1e-3, "strategies": ['CC', 'HD', 'TD']},
-    {"b": 1.5, "c": 0.5, "d": 0.3, "s": 0.2,
-     "Z": 100, "beta": 1, "mu": 1e-3, "strategies": ['CC', 'HD', 'TD']},
-    {"b": 1.5, "c": 0.5, "d": 0.1, "s": 0.1,
-     "Z": 100, "beta": 1, "mu": 1e-3, "strategies": ['CC', 'HD', 'TD']},
-    {"b": 1.5, "c": 0.5, "d": 0.2, "s": 0.1,
-     "Z": 100, "beta": 1, "mu": 1e-3, "strategies": ['CC', 'HD', 'TD']},
-    {"b": 1.5, "c": 0.5, "d": 0.3, "s": 0.1,
-     "Z": 100, "beta": 1, "mu": 1e-3, "strategies": ['CC', 'HD', 'TD']},
+     "Z": 100, "beta": 1, "mu": 1e-3, "strategies": ['CC', 'HD', 'TD'],
+     "hardcode_stability": True,
+     "payoffs_fn": core.tactical_deception_payoffs_s_curve},
+    ## Uncomment below to generate additional figures.
+    # {"b": 1.5, "c": 0.5, "d": 0.1, "s": 0.2,
+    #  "bias": 0.3,
+    #  "Z": 100, "beta": 1, "mu": 1e-3, "strategies": ['CC', 'HD', 'TD'],
+    #  "payoffs_fn": core.tactical_deception_payoffs_s_curve},
+    # {"b": 1.5, "c": 0.5, "d": 0.1, "s": 0.2,
+    #  "bias": 0.7,
+    #  "Z": 100, "beta": 1, "mu": 1e-3, "strategies": ['CC', 'HD', 'TD'],
+    #  "payoffs_fn": core.tactical_deception_payoffs_s_curve},
 ]
 
 simulation_fn = None # Currently, no simulation_fn exists
 # plotting_fn = core.plot_simplex # ~30s per plot
-# plotting_fn = core.plot_simplex_numerical # ~150s per plot
-plotting_fn = core.plot_deception_frequency
-plotting_fn = core.plot_strategy_frequencies
+plotting_fn = core.plot_simplex_numerical # ~150s per plot
+# plotting_fn = core.plot_deception_frequency
+# plotting_fn = core.plot_strategy_frequencies
+# plotting_fn = core.plot_conflict_comparison
 results = core.run_all_simulations(param_list,
                                    simulation_fn=simulation_fn,
                                    plotting_fn=plotting_fn,
@@ -28,3 +30,11 @@ results = core.run_all_simulations(param_list,
                                    simulation_dir="dataframes",)
 
 figs, simulation_results = results
+
+results2 = core.run_all_simulations(param_list,
+                                   simulation_fn=simulation_fn,
+                                   plotting_fn=core.plot_conflict_comparison,
+                                   plot_dir="plots",
+                                   simulation_dir="dataframes",)
+
+figs2, simulation_results2 = results2
